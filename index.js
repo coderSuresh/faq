8e7ab2375649c70f4af3d92fa107c242583e4fe1
Eyu-Gen <dreamersthebig@gmail.com>
2024-05-19 20:00:57 +0545

    Incomplete

---
+++ index.js
@@ -0,0 +1,7 @@
+let button1 = document.getElementsByClassName("plusLogo");
+let button2 = document.getElementsByClassName("minusLogo");
+let block = document.getElementsByClassName("block");
+
+console.log(button1);
+console.log(button2);
+console.log(block);
